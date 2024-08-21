e0f938bb9ac79bdffcf0afe7f940c56c0cc80a39
saquib <saquibhussain509@gmail.com>
2024-08-21 11:20:02 +0530

    Updates

---
+++ precache-manifest.4ba69e2b480fdef8e2511869dc64d8a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4205a7859f0dfb5f4ac218f2ee76441c",
+    "url": "/reactdemo/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/reactdemo/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "8519cee2facf9d97182d",
+    "url": "/reactdemo/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/reactdemo/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "8519cee2facf9d97182d",
+    "url": "/reactdemo/static/js/main.63275ce7.chunk.js"
+  },
+  {
+    "revision": "d63cc1b5485118404932",
+    "url": "/reactdemo/static/js/runtime~main.dbcbabdd.js"
+  }
+]);
